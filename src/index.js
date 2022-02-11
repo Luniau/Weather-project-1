9c033e4be910679ecf0f10e3b9fff26f6299abc6
Luniau <lubecarvalho@hotmail.com>
2022-02-11 11:39:17 +0100

    Forecast data works using API

--- src/index.js
+++ src/index.js
@@ -42,28 +42,43 @@ function formatDate(timestamp) {
   return `${day}, ${hours}: ${minutes}`;
 }
 
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let day= date.getDay();
+   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+   return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+ let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+ 
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6){
+      forecastHTML =
+        forecastHTML +
+        `
      <div class="col-2">
-       <div class="weather-forecast-date">${day}</div>
+       <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
        <img
-         src="http://openweathermap.org/img/wn/50d@2x.png"
+         src="http://openweathermap.org/img/wn/${
+           forecastDay.weather[0].icon
+         }@2x.png"
          alt=""
          width="42"
        />
        <div class="weather-forecast-temperature">
-         <span class="weather-forecast-temperature-max">18°</span>
-         <span class="weather-forecast-temperature-min">12°</span>
+         <span class="weather-forecast-temperature-max">${Math.round(
+           forecastDay.temp.max
+         )}°</span>
+         <span class="weather-forecast-temperature-min">${Math.round(
+           forecastDay.temp.min
+         )}°</span>
        </div>
      </div>
    `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -71,10 +86,10 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log (coordinates);
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayForecast);
+  console.log(coordinates);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -101,7 +116,6 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 
-
   getForecast(response.data.coord);
 }
 
@@ -127,7 +141,6 @@ function displayCelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
-
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayfahrenheitTemperature);
